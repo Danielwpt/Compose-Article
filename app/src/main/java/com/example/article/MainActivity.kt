82ab76a4711fb660054e49dbe49fe671bb9ee04b
Daniel <danielwong151999@gmail.com>
2025-03-28 17:01:53 +0800

    + Added image for article + Added text title and description of the article

---
+++ app/src/main/java/com/example/article/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.article
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.article.ui.theme.ArticleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ArticleTheme {
+                setContent {
+                    enableEdgeToEdge()
+                    Column {
+                        DisplayArticleImage()
+                        DisplayArticleContents()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DisplayArticleImage(modifier : Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.bg_compose_background),
+        contentDescription = stringResource(R.string.article_background_description),
+        modifier = modifier.fillMaxWidth()
+    )
+}
+
+@Composable
+fun DisplayArticleContents(modifier: Modifier = Modifier) {
+    Text(text = stringResource(id = R.string.article_title), modifier.padding(16.dp), fontSize = 24.sp)
+    Text(text = stringResource(id = R.string.article_first_description), modifier.padding(16.dp, 0.dp, 16.dp, 0.dp), textAlign = TextAlign.Justify)
+    Text(text = stringResource(id = R.string.article_second_description), modifier.padding(16.dp), textAlign = TextAlign.Justify)
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun DisplayArticleImagePreview(modifier : Modifier = Modifier) {
+    Column {
+        DisplayArticleImage()
+        DisplayArticleContents()
+    }
+}
